f5bee3a0804b2929d9fc5b7e7d63ce5f635d201a
N Aditiya <94776808+Lucifwr233@users.noreply.github.com>
2021-12-24 08:28:34 +0700

    Create Array2D.java

---
+++ Latihan & dll/202153032_Aditiya/Array2D.java
@@ -0,0 +1,12 @@
+public class DuaDimensi {
+    public static void main(String[] args) {
+        //mendeklarasikan variabel nama dengan tipe data Array
+        String[][] nama ={
+            {"Pak ", "Bu ", "Mbak "},
+            {"muhammad ", "santi", "lili"},
+        };
+        System.out.println(nama[0][0] + nama[1][0]);
+        System.out.println(nama[0][1] + nama[1][1]);
+        System.out.println(nama[0][2] + nama[1][2]);
+    }
+}
