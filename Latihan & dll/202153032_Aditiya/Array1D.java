cf2c08371b758e482dd19ab140ddfa863e297531
N Aditiya <94776808+Lucifwr233@users.noreply.github.com>
2021-12-24 08:29:23 +0700

    Create Array1D.java

---
+++ Latihan & dll/202153032_Aditiya/Array1D.java
@@ -0,0 +1,15 @@
+public class SatuDimensi {
+    public static void main(String[] args) {
+        //mendeklarasikan variabel kota dengan tipe data Array
+        String[] kota;
+        kota = new String[3];
+        //mengisi elemen array
+        kota[0] = "Jakarta";
+        kota[1] = "Semarang";
+        kota[2] = "Kudus";
+        //menampilkan isi array
+        System.out.println(kota[0]);
+        System.out.println(kota[1]);
+        System.out.println(kota[2]);
+    }
+}
